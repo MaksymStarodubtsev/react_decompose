9441477342a7cbddad3ba93a1746f0760a1a19b4
Maksym Starodubtsev <maksym.starodubtsev@gmail.com>
2021-07-15 11:02:21 +0300

    Updates

---
+++ precache-manifest.9f733532235d505b295e6e8e3aa10529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2395f79a121d9a5a1e034a85bb4ffe6e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a14de743d4ae9b75105",
+    "url": "./static/css/main.56d82817.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a14de743d4ae9b75105",
+    "url": "./static/js/main.b24c4e0d.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
